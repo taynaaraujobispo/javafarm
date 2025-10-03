e2a91e8e1619f97adceaeead8cc65029417d163a
Tayná Araújo Bispo <taynaaraujobispo@gmail.com>
2025-10-03 20:33:32 -0300

    Update Batata.java

--- src/main/java/br/com/joaocarloslima/Batata.java
+++ src/main/java/br/com/joaocarloslima/Batata.java
@@ -2,6 +2,6 @@ package br.com.joaocarloslima;
 
 public class Batata extends Planta {
     public Batata() {
-        super("batata", 3); // tempo de crescimento = 3 (ajuste se quiser)
+        super("batata", 3); // tempo de crescimento = 3 
     }
 }
